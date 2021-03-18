773fa97de30e30113233974c116e40deb3f22446
Laura Leinweber <2478354L@student.gla.ac.uk>
2021-03-18 22:44:07 +0000

    index, signup, signin + views + urls

---
+++ LinkedOnApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from LinkedOnApp import views
+
+app_name = 'LinkedOn'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+]
--- LinkedOnApp/views.py
+++ LinkedOnApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    response = render(request, 'LinkedOn/index.html')
+    return response
+    
+def signin(request):
+    return render(request, 'LinkedOn/signin.html')
+    
+def signup(request):
+    return render(request, 'LinkedOn/signup.html')
+
+    
