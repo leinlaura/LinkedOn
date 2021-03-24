6e605a2589c99fde9ec832bd05e80dca11444b7a
Laura Leinweber <2478354L@student.gla.ac.uk>
2021-03-24 03:08:52 +0000

    mods sign up and about page

---
+++ static/js/aboutus.js
@@ -0,0 +1,17 @@
+const employer = document.querySelector('#employer');
+const jobseeker = document.querySelectorAll('#jobseeker');
+
+employer.addEventListener("click", disable);
+jobseeker.addEventListener("click", enable);
+
+function disable(){
+	document.getElementById("sentences").disabled = true;
+		document.getElementById("describe").disabled = true;
+		document.getElementById("formFileSm").disabled = true;
+}
+
+function enable(){
+	document.getElementById("sentences").disabled = false;
+	document.getElementById("describe").disabled = false;
+	document.getElementById("formFileSm").disabled = false;
+}
