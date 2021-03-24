066b179ee905b5cf03a5da088fe6db931a47a737
Alistair Hampton <2459255H'student.gla.ac.uk>
2021-03-24 19:04:53 +0000

    Added signup support.

--- LinkedOnApp/forms.py
+++ LinkedOnApp/forms.py
@@ -5,13 +5,13 @@ from LinkedOnApp.models import UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password','firstname', 'lastname',)
-        
-        
+        fields = ('username', 'password', 'first_name', 'last_name',)
+
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'company', 'profileImage', 'about', 'searchingInfo', 'isEmployer',)
-
--- LinkedOnApp/views.py
+++ LinkedOnApp/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from LinkedOnApp.forms import UserForm, UserProfileForm
+
 
 def index(request):
     return render(request, 'LinkedOn/index.html')
-    
+
+
 def signin(request):
     return render(request, 'LinkedOn/signin.html')
-    
+
+
 def signup(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save(commit=False)
+            user.user_name = user.email
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            if 'profileImage' in request.FILES:
+                profile.profileImage = request.FILES['profileImage']
+
+            profile.save()
+
     return render(request, 'LinkedOn/signup.html')
-    
+
+
 def aboutus(request):
     return render(request, 'LinkedOn/aboutus.html')
-    
+
+
 def categories(request):
     return render(request, 'LinkedOn/categories.html')
-    
+
+
 def joblisting(request):
     return render(request, 'LinkedOn/joblisting.html')
-    
+
+
 def profile(request):
     return render(request, 'LinkedOn/profile.html')
-    
-
-    
