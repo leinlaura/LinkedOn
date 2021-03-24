ef4d7c53c24e60fdd2b40746d2a8b72ecb90cd42
Sandy Millar <2309647m@student.gla.ac.uk>
2021-03-24 18:03:26 +0000

    population script created

--- LinkedOnApp/models.py
+++ LinkedOnApp/models.py
@@ -1,16 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+            
+class Category(models.Model):
+    NAME_MAX_LENGTH = 128
+    
+    name = models.CharField(max_length=NAME_MAX_LENGTH, 
+                            unique=True)
+    slug = models.SlugField()
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
     
 class UserProfile(models.Model):
     ABOUT_MAX_LENGTH = 1000
     SEARCHING_MAX_LENGTH = 1000
     WEBSITE_MAX_LENGTH = 128
+    COMPANY_MAX_LENGTH = 128
     
     #user includes username, password, email, firstname, lastname.
     #Could have users input their email as the 'username'
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
-    #category : still figuring this out. will get back to it
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     website = models.CharField(max_length=WEBSITE_MAX_LENGTH, blank=True)
+    company = models.CharField(max_length= COMPANY_MAX_LENGTH, blank=True)
     about = models.CharField(max_length=ABOUT_MAX_LENGTH, blank=True)
     searchingInfo = models.CharField(max_length=SEARCHING_MAX_LENGTH, blank=True)
     profileImage = models.ImageField(blank=True)
@@ -24,17 +44,8 @@ class JobListing(models.Model):
     DESCRIPTION_MAX_LENGTH = 1000
     
     job_id = models.CharField(max_length=ID_MAX_LENGTH, unique=True)
-    employer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    employer = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH)
     
     def __str__(self):
         return self.job_id
-        
-class Category(models.Model):
-    NAME_MAX_LENGTH = 128
-    
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
-    
-    def __str__(self):
-        return self.name
-    
