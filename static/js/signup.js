8d4b46d40138db3fbc3caab838879dd01854ab66
Alistair Hampton <2459255H'student.gla.ac.uk>
2021-03-27 17:52:54 +0000

    Changed submit positioning.

--- static/js/signup.js
+++ static/js/signup.js
@@ -4,11 +4,13 @@ $(function () {
         $("#right").show();
         $('#formFileSm').show();
         $('#formCompany').hide();
+        $('#submit').appendTo('#right')
     });
 
     $("#employer").click(function () {
         $("#right").hide();
         $('#formFileSm').hide();
         $('#formCompany').show();
+        $('#submit').appendTo('#middle')
     });
 });
