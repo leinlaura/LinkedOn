289ab222be818872b51284c0973579edb7f705d9
Laura Leinweber <2478354L@student.gla.ac.uk>
2021-03-27 21:46:11 +0000

    views.py show_category fix

--- LinkedOnApp/views.py
+++ LinkedOnApp/views.py
@@ -1,4 +1,4 @@
-from LinkedOnApp.models import Category
+from LinkedOnApp.models import Category, UserProfile
 from django.shortcuts import render
 from LinkedOnApp.forms import UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
@@ -83,30 +83,26 @@ def categories(request):
 def show_category(request, category_name_slug):
     context_dic = {}
     
-    if request.user.is_authenticated():
-        try:
-            category = Category.objects.get(slug=category_name_slug)
-            context_dic['category']= category
-            #get profiles where the category matches and they are not employers
-            profiles = UserProfile.objects.filter(category =category, isEmployer = False)
-            profileNumber = profiles.count() #number of profiles in that category
-            length_of_col = math.ceil(profileNumber/3)  #number of profiles in a column
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dic['category']= category
+        #get profiles where the category matches and they are not employers
+        profiles = UserProfile.objects.filter(category =category, isEmployer = False)
+        profileNumber = profiles.count() #number of profiles in that category
+        length_of_col = math.ceil(profileNumber/3)  #number of profiles in a column
             
-            #three colummns
-            context_dic["first_col"]= profiles[:length_of_col]
-            context_dic["second_col"] = profiles[length_of_col:2*length_of_col]
-            context_dic["third_col"]= profiles[2*length_of_col:]
+        #three colummns
+        context_dic["first_col"]= profiles[:length_of_col]
+        context_dic["second_col"] = profiles[length_of_col:2*length_of_col]
+        context_dic["third_col"]= profiles[2*length_of_col:]
             
             
-            #context_dic["profileNumber"] = profileNumber
-            context_dic["profiles"] = profiles
+        #context_dic["profileNumber"] = profileNumber
+        context_dic["profiles"] = profiles
             
-        except Category.DoesNotExist:
-            context_dic['category']=None
-            context_dic["profiles"]=None
-    
-    else:
-        return redirect(reverse('LinkedOn:signin'))
+    except Category.DoesNotExist:
+        context_dic['category']=None
+        context_dic["profiles"]=None
         
     return render(request, 'LinkedOn/show_category.html', context_dic)
 
