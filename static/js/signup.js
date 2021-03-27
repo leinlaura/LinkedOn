1a362c3c1407c2c3065f94a17a542807d46e2d83
Alistair Hampton <2459255H'student.gla.ac.uk>
2021-03-27 17:36:09 +0000

    Hide unnecessary form fields for employers and changed css of signup.html.

--- static/js/signup.js
+++ static/js/signup.js
@@ -2,9 +2,12 @@ $(function () {
 
     $("#jobseeker").click(function () {
         $("#right").show();
+        $('#formCompany').hide();
     });
 
     $("#employer").click(function () {
         $("#right").hide();
+        $('#formFileSm').hide();
+        $('#formCompany').show();
     });
 });
