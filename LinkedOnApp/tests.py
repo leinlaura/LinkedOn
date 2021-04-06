d3c8111a09fc63c099d5637f75567cc2cf78baa2
Laura Leinweber <2478354L@student.gla.ac.uk>
2021-04-06 17:30:52 +0100

    test clean

--- LinkedOnApp/tests.py
+++ LinkedOnApp/tests.py
@@ -145,7 +145,7 @@ class FormTesting(TestCase):
 
 class LinkedOn_TemplatesStructureTests(TestCase):
     """
-    Have you set templates, static files and media files up correctly, as per the book?
+    are templates, static and media files set up correctly
     """
 
     def setUp(self):
@@ -162,14 +162,14 @@ class LinkedOn_TemplatesStructureTests(TestCase):
 
     def test_template_dir_setting(self):
         """
-        Does the TEMPLATE_DIR setting exist, and does it point to the right directory?
+        Does the TEMPLATE_DIR setting exist in the right directory?
         """
         variable_exists = 'TEMPLATE_DIR' in dir(settings)
-        self.assertTrue(variable_exists, f"{FAILURE_HEADER}Your settings.py module does not have the variable TEMPLATE_DIR defined!{FAILURE_FOOTER}")
+        self.assertTrue(variable_exists, f"{FAILURE_HEADER}TEMPLATE_DIR not defined.{FAILURE_FOOTER}")
 
         template_dir_value = os.path.normpath(settings.TEMPLATE_DIR)
         template_dir_computed = os.path.normpath(self.templates_dir)
-        self.assertEqual(template_dir_value, template_dir_computed, f"{FAILURE_HEADER}Your TEMPLATE_DIR setting does not point to the expected path. Check your configuration, and try again.{FAILURE_FOOTER}")
+        self.assertEqual(template_dir_value, template_dir_computed, f"{FAILURE_HEADER}TEMPLATE_DIR not expected path.{FAILURE_FOOTER}")
 
     def test_template_lookup_path(self):
         """
@@ -184,7 +184,7 @@ class LinkedOn_TemplatesStructureTests(TestCase):
             if entry_normalised == os.path.normpath(settings.TEMPLATE_DIR):
                 found_path = True
 
-        self.assertTrue(found_path, f"{FAILURE_HEADER}Your project's templates directory is not listed in the TEMPLATES>DIRS lookup list. Check your settings.py module.{FAILURE_FOOTER}")
+        self.assertTrue(found_path, f"{FAILURE_HEADER}not in template dir lookup{FAILURE_FOOTER}")
 
 
 class LinkedOn_DatabaseConfigurationTests(TestCase):
@@ -302,17 +302,6 @@ class LinkedOn_Show_CategoryViewsTests(TestCase):
 
         self.assertEquals('this-is-new', category.slug, f"{FAILURE_HEADER}the slug attribute was not updated correctly. {FAILURE_FOOTER}")
 
-    def test_context_dictionary(self):
-        """
-        tests whether the context dictionary is passed correctly
-        """
-        data_science_category = Category.objects.get_or_create(name='Data Science')
-        profiles_list = list(UserProfile.objects.filter(category=data_science_category))
-        
-       # self.assertTrue('category' in self.response.context, f"{FAILURE_HEADER}The 'category' variable is not in the context dictionary{FAILURE_FOOTER}")
-        self.assertTrue('profiles' in self.response.context, f"{FAILURE_HEADER}The 'profiles' category is not in the context_dictionary{FAILURE_FOOTER}")
-    
-  
     
     
     
